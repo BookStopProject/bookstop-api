984cce1f1cd1acb98d2941f2f21a3c6194b4f5e2
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2022-11-24 11:35:37 +0700

    Update schema

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,134 +1,101 @@
-CREATE TABLE public."user"
-(
-    id serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
-    oauth_id character varying(50) NOT NULL,
-    email character varying(256),
-    name character varying(100) NOT NULL,
-    description character varying(160),
-    "profile_image_url" character varying,
-    credit integer NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+CREATE TABLE public."book" (
+    id serial PRIMARY KEY,
+    title varchar(256) NOT NULL,
+    subtitle varchar(256),
+    description text,
+    published_year integer NOT NULL,
+    genre_id integer,
+    tradein_credit integer NOT NULL,
+    exchange_credit integer NOT NULL,
+    FOREIGN KEY (genre_id) REFERENCES public."genre" (id) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
 
-CREATE TABLE public.browse
-(
-    id serial NOT NULL,
-    name character varying(100) NOT NULL,
-    description character varying(160),
-    started_at timestamp without time zone NOT NULL,
-    ended_at timestamp without time zone NOT NULL,
-    image_url character varying,
-    PRIMARY KEY (id)
+CREATE TABLE public."book_author" (
+    book_id integer,
+    author_id integer,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES public."book" (id) ON UPDATE NO ACTION ON DELETE RESTRICT,
+    FOREIGN KEY (author_id) REFERENCES public."author" (id) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
 
-CREATE TABLE public.browse_book
-(
-    book_id character varying(21) NOT NULL,
-    browse_id integer NOT NULL,
-    FOREIGN KEY (browse_id)
-        REFERENCES public.browse (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-        NOT VALID
+CREATE TABLE public."author" (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    description varchar(160),
+    date_of_birth date varying(200) NOT NULL,
+    date_of_death date varying(200) NOT NULL,
+    nationality varchar(200) NOT NULL
 );
 
-CREATE TABLE public.location
-(
-    id serial NOT NULL,
-    name character varying(100) NOT NULL,
-    parent_name character varying(200),
-    address_line character varying NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE public."genre" (
+    id serial PRIMARY KEY,
+    name varchar(64) NOT NULL,
+    description text NOT NULL
 );
 
-CREATE TABLE public.user_book
-(
-    id serial NOT NULL,
+CREATE TYPE book_condition AS ENUM (
+    'new',
+    'like_new',
+    'very_good',
+    'acceptable'
+);
+
+CREATE TABLE pubic."book_copy" (
+    id serial PRIMARY KEY,
+    book_id integer,
+    condition book_condition,
+    FOREIGN KEY (book_id) REFERENCES public."book" (id) ON UPDATE NO ACTION ON DELETE RESTRICT
+);
+
+CREATE TABLE public."user" (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    bio varchar(160),
+    profile_picture varchar,
+    creation_time timestamp WITHOUT time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+    credit integer NOT NULL DEFAULT 0
+);
+
+CREATE TABLE public."user_book" (
+    id serial PRIMARY KEY,
+    book_id integer NOT NULL,
     user_id integer NOT NULL,
-    book_id character varying NOT NULL,
-    started_at date,
-    ended_at date,
-    id_original integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id)
-        REFERENCES public."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    FOREIGN KEY (id_original)
-        REFERENCES public.user_book (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    start_date date,
+    end_date date,
+    FOREIGN KEY (book_id) REFERENCES public."book" (id) ON UPDATE NO ACTION ON DELETE RESTRICT,
+    FOREIGN KEY (user_id) REFERENCES public."user" (id) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-
-CREATE TABLE public.inventory
-(
-    id serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
-    removed_at timestamp without time zone,
-    user_book_id integer NOT NULL,
-    location_id integer NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_book_id)
-        REFERENCES public.user_book (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    FOREIGN KEY (location_id)
-        REFERENCES public.location (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    UNIQUE(user_book_id)
-);
-
-CREATE TABLE public.exchange
-(
-    id serial NOT NULL,
-    user_book_id_old integer NOT NULL,
-    user_book_id_new integer NOT NULL,
-    user_book_id_original integer NOT NULL,
-    exchanged_at timestamp without time zone NOT NULL,
-    location_id integer,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_book_id_old)
-        REFERENCES public.user_book (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    FOREIGN KEY (user_book_id_new)
-        REFERENCES public.user_book (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    FOREIGN KEY (user_book_id_original)
-        REFERENCES public.user_book (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    FOREIGN KEY (location_id)
-        REFERENCES public.location (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-);
-
-CREATE TABLE public.inventory_claim
-(
-    id serial NOT NULL,
+CREATE TABLE public."post" (
+    id serial PRIMARY KEY,
+    text varchar(250),
+    creation_time timestamp NOT NULL,
+    book_id integer NOT NULL,
     user_id integer NOT NULL,
-    inventory_id integer NOT NULL,
-    claimed_at timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id)
-        REFERENCES public."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    FOREIGN KEY (inventory_id)
-        REFERENCES public.inventory (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
+    is_recommending boolean NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES public."book" (id) ON UPDATE NO ACTION ON DELETE RESTRICT,
+    FOREIGN KEY (user_id) REFERENCES public."user" (id) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
+
+CREATE TABLE public."location" (
+    id serial PRIMARY KEY name varchar(128) NOT NULL,
+    address varchar(512) NOT NULL,
+);
+
+CREATE TABLE public."exchange" (
+    id serial PRIMARY KEY,
+    book_copy_id integer,
+    previous_user_id integer,
+    next_user_id integer,
+    exchange_time timestamp WITHOUT time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+    FOREIGN KEY (previous_user_id) REFERENCES public."user" (id) ON UPDATE NO ACTION ON DELETE RESTRICT,
+    FOREIGN KEY (next_user_id) REFERENCES public."user" (id) ON UPDATE NO ACTION ON DELETE RESTRICT
+);
+
+CREATE TABLE public."inventory_entry" ();
+
+CREATE TABLE public."event_inventory_entry" ();
+
+CREATE TABLE public."invoice" ();
+
+CREATE TABLE public."invoice_entry" ();
+
