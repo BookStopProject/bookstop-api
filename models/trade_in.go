941d54ca17dfd21a20e4dd060f58e5d757f42e82
Duong Tran <tranhoaiduong136@gmail.com>
2022-12-07 23:39:52 +0700

    Finnish Trade In

--- models/trade_in.go
+++ models/trade_in.go
@@ -1,9 +1,12 @@
 package models
 
 import (
-	"bookstop/db"
 	"context"
+	"database/sql"
+	"errors"
 	"time"
+
+	"bookstop/db"
 )
 
 type TradeIn struct {
@@ -26,7 +29,7 @@ func FindTradeInsByUserID(ctx context.Context, userId int) ([]*TradeIn, error) {
 		ti.creation_time,
 		b.id,
 		b.title,
-		b.subtitle
+		b.subtitle,
 		b.published_year,
 		a.id,
 		a.name
@@ -83,5 +86,64 @@ func DoTradeIn(ctx context.Context, userBookID int, condition BookCondition, loc
 	//		the book trade in value * condition multiplier (see book_copy.go).
 	// 4) Add the credit to the user's credit balance
 	// 5) Return the trade in
-	return nil, nil
+
+	var tradeIn *TradeIn
+	tradeIn.Book = new(Book)
+	var err error
+
+	getBookCopyIDQuery := `SELECT book_copy_id, user_id FROM user_book WHERE id=$1`
+
+	getUserBookQuery := `SELECT * FROM book JOIN user_book ub ON book.id = ub.book_id`
+
+	updateBookCopyQuery := `UPDATE book_copy SET condition=$1 && location_id=$2 WHERE id=$3`
+
+	insertTradeInQuery := `INSERT INTO trade_in(id, user_id, book_copy_id, credit) VALUES ($1, $2, $3, $4)`
+
+	if err := db.Conn.QueryRow(ctx, getUserBookQuery).Scan(
+		&tradeIn.Book.ID,
+		&tradeIn.Book.Title,
+		&tradeIn.Book.Subtitle,
+		&tradeIn.Book.PublishedYear,
+		&tradeIn.Book.Author.ID,
+		&tradeIn.Book.Author.Name,
+	); err != nil {
+		return nil, err
+	}
+
+	err = db.Conn.QueryRow(ctx, getBookCopyIDQuery).Scan(&tradeIn.BookCopyID, &tradeIn.UserID)
+	if errors.Is(err, sql.ErrNoRows) {
+		insertQuery := `INSERT INTO book_copy(id, book_id, condition, location_id) VALUES ($1, $2, $3, $4)`
+		if _, err := db.Conn.Exec(ctx, insertQuery, tradeIn.BookCopyID, tradeIn.Book.ID, condition, locationID);  err != nil {
+			return nil, err
+		}
+
+	} else if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Conn.Exec(ctx, updateBookCopyQuery, condition, locationID, tradeIn.BookCopyID)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Conn.Exec(ctx, insertTradeInQuery, tradeIn.BookCopyID, tradeIn.UserID, tradeIn.BookCopyID, float64(tradeIn.Book.TradeinCredit) * getBookConditionValue(condition))
+	if err != nil {
+		return nil, err
+	}
+
+
+	return tradeIn, nil
+}
+
+func getBookConditionValue(bc BookCondition) float64 {
+	switch bc {
+	case BookConditionNew:
+		return BookConditionNewMultiplier
+	case BookConditionLikeNew:
+		return BookConditionLikeNewMultiplier
+	case BookConditionGood:
+		return BookConditionGoodMultiplier
+	case BookConditionAcceptable:
+		return BookConditionAcceptableMultiplier
+	}
 }
